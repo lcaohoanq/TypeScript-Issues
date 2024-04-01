bb902ab9039a4cf6bd5b8bc489dee3dbe38d43cb
lcaohoanq <hoangclw@gmail.com>
2024-04-01 09:56:15 +0700

    Working with number, string, boolean

--- Understanding-TypeScript/02-TypeScript-Basics-And-Basics-Types/002-Using-Types/app.js
+++ Understanding-TypeScript/02-TypeScript-Basics-And-Basics-Types/002-Using-Types/app.js
@@ -1,4 +1,11 @@
-var sum = function (num1, numb2) { return num1 + numb2; };
+function sum(num1, num2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + num1 + num2);
+    }
+    return num1 + num2;
+}
 var number1 = 5;
 var number2 = 2.8;
-console.log(sum(number1, number2));
+var showResult = false;
+var resultPhrase = "Result is: ";
+console.log(sum(number1, number2, showResult, resultPhrase));
