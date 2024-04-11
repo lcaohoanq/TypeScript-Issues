57da5ce56ed60dd90a50b98dc898e6a50a91751b
lcaohoanq <hoangclw@gmail.com>
2024-04-11 09:06:13 +0700

    feat: add Validatable interface

--- Understanding-TypeScript/09-Practice-Time-Build-Drag-Drop-Project/src/app.ts
+++ Understanding-TypeScript/09-Practice-Time-Build-Drag-Drop-Project/src/app.ts
@@ -1,3 +1,47 @@
+//validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 //auto bind decorator
 function autoBind(
   target: any,
@@ -51,20 +95,35 @@ class ProjectInput {
     this.attach();
   }
 
-  private validate(title: string, desc: string, people: number) {
-    if (title.trim().length === 0 || desc.trim().length === 0 || people <= 0) {
-      return false;
-    }
-    return true;
-  }
-
   //the return type is tuple
   private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    if (!this.validate(enteredTitle, enteredDescription, +enteredPeople)) {
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
       alert("Invalid input, please try again!");
       return;
     } else {
