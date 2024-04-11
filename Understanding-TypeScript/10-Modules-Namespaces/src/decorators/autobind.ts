8035e51d5e7ee7711cbb09ad75c2869fe5388772
lcaohoanq <hoangclw@gmail.com>
2024-04-11 14:17:59 +0700

    refactor: implements namespace App

---
+++ Understanding-TypeScript/10-Modules-Namespaces/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+namespace App {
+  //auto bind decorator
+  export function autoBind(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
