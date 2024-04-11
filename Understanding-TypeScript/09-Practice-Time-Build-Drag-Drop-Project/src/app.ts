f57a32b6b7aee93da6db74fd252fde3b229bd0e0
lcaohoanq <hoangclw@gmail.com>
2024-04-11 10:38:29 +0700

    feat: add getter for retrive the data correctly

--- Understanding-TypeScript/09-Practice-Time-Build-Drag-Drop-Project/src/app.ts
+++ Understanding-TypeScript/09-Practice-Time-Build-Drag-Drop-Project/src/app.ts
@@ -187,8 +187,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
